f846e25f9011a5bd1b98b2b9059c608cb08611b3
marshallhch <marshallhch@gmail.com>
2024-10-04 10:52:54 +0900

    ec2 deploy test

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const path = require('path');
 const spawn = require('child_process').spawn;
 const port = 8080;
 const app = express();
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+const isDevelopment = () => {
+  return process.env.NODE_ENV === 'development';
+};
+
+const pythonExePath = isDevelopment()
+  ? path.join('C:', 'conda', 'envs', 'recom_env', 'python.exe')
+  : path.join('/home/ubuntu/miniconda', 'envs', 'myenv', 'bin', 'python3');
 
 app.get('/', (req, res) => {
   res.send('Hello from Node server!');
@@ -15,20 +24,9 @@ app.get('/', (req, res) => {
 app.get('/random/:count', (req, res) => {
   try {
     const scriptPath = path.join(__dirname, 'resolver.py');
-    // const pythonPath = path.join(
-    //   'C:',
-    //   'conda',
-    //   'envs',
-    //   'recon_env',
-    //   'python.exe'
-    // );
-
-    // test 1
-
-    const pythonPath = path.join('/home/ubuntu/miniconda', 'envs', 'myenv', 'bin', 'python3');
 
     const count = req.params.count;
-    const result = spawn(pythonPath, [scriptPath, 'random', count]);
+    const result = spawn(pythonExePath, [scriptPath, 'random', count]);
 
     let responseData = '';
 
@@ -58,18 +56,9 @@ app.get('/random/:count', (req, res) => {
 app.get('/latest/:count', (req, res) => {
   try {
     const scriptPath = path.join(__dirname, 'resolver.py');
-    // const pythonPath = path.join(
-    //   'C:',
-    //   'conda',
-    //   'envs',
-    //   'recon_env',
-    //   'python.exe'
-    // );
-
-    const pythonPath = path.join('/home/ubuntu/miniconda', 'envs', 'myenv', 'bin', 'python3');
 
     const count = req.params.count;
-    const result = spawn(pythonPath, [scriptPath, 'latest', count]);
+    const result = spawn(pythonExePath, [scriptPath, 'latest', count]);
 
     let responseData = '';
 
@@ -99,19 +88,10 @@ app.get('/latest/:count', (req, res) => {
 app.get('/genres/:genre/:count', (req, res) => {
   try {
     const scriptPath = path.join(__dirname, 'resolver.py');
-    // const pythonPath = path.join(
-    //   'C:',
-    //   'conda',
-    //   'envs',
-    //   'recon_env',
-    //   'python.exe'
-    // );
-
-    const pythonPath = path.join('/home/ubuntu/miniconda', 'envs', 'myenv', 'bin', 'python3');
 
     const genre = req.params.genre;
     const count = req.params.count;
-    const result = spawn(pythonPath, [scriptPath, 'genres', genre, count]);
+    const result = spawn(pythonExePath, [scriptPath, 'genres', genre, count]);
 
     let responseData = '';
 
@@ -141,18 +121,9 @@ app.get('/genres/:genre/:count', (req, res) => {
 app.get('/item-based/:item', (req, res) => {
   try {
     const scriptPath = path.join(__dirname, 'recommender.py');
-    // const pythonPath = path.join(
-    //   'C:',
-    //   'conda',
-    //   'envs',
-    //   'recon_env',
-    //   'python.exe'
-    // );
-
-    const pythonPath = path.join('/home/ubuntu/miniconda', 'envs', 'myenv', 'bin', 'python3');
 
     const item = req.params.item;
-    const result = spawn(pythonPath, [scriptPath, 'item-based', item]);
+    const result = spawn(pythonExePath, [scriptPath, 'item-based', item]);
 
     let responseData = '';
 
@@ -178,52 +149,6 @@ app.get('/item-based/:item', (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 });
-
-app.post('/user-based', (req, res) => {
-  try {
-    const scriptPath = path.join(__dirname, 'recommender.py');
-    // const pythonPath = path.join(
-    //   'C:',
-    //   'conda',
-    //   'envs',
-    //   'recon_env',
-    //   'python.exe'
-    // );
-
-    const pythonPath = path.join('/home/ubuntu/miniconda', 'envs', 'myenv', 'bin', 'python3');
-
-    const inputRatingDict = req.body; // JSON 데이터를 받아옴
-
-    const result = spawn(pythonPath, [scriptPath, 'user-based']);
-
-    let responseData = '';
-
-    // 파이썬 스크립트로 JSON 데이터를 전달
-    result.stdin.write(JSON.stringify(inputRatingDict));
-    result.stdin.end();
-
-    result.stdout.on('data', function (data) {
-      responseData += data.toString();
-    });
-
-    result.on('close', (code) => {
-      if (code === 0) {
-        const jsonResponse = JSON.parse(responseData);
-        res.status(200).json(jsonResponse);
-      } else {
-        res
-          .status(500)
-          .json({ error: `Child process exited with code ${code}` });
-      }
-    });
-
-    result.stderr.on('data', (data) => {
-      console.error(`stderr: ${data}`);
-    });
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
-})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
